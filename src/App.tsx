1555da36facdca601d82a4268a58149f67da87f0
Artem <example@gmail.com>
2022-07-30 20:15:37 +0300

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useState} from 'react';
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
+import { Loader } from "./components/Loader";
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Modal } from "./components/Modal";
+import { CreateProduct } from "./components/CreateProduct";
+import {IProduct} from "./models";
+import {ModalContext} from "./context/ModalContext";
 
 function App() {
+  const { error, loading, products, addProduct } = useProducts()
+  const { modal, open, close } = useContext(ModalContext);
+
+  const createHandler = (product: IProduct) => {
+    close()
+    addProduct(product)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'container mx-auto max-w-2xl pt-5'}>
+      { error && <ErrorMessage error={error} />}
+      { loading && <Loader />}
+      { products.map((product) => <Product product={product} key={product.id} />) }
+
+      {modal && <Modal onClose={close} title={'New Product'}>
+        <CreateProduct onCreate={createHandler}/>
+      </Modal>}
+
+      <button onClick={open} className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'>+</button>
     </div>
   );
 }
